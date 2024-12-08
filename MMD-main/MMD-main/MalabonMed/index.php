351401092751822fa357202e6196868a2b0833bc
Jinjaaa <132412471+Jinjaaa@users.noreply.github.com>
2024-12-08 23:52:47 +0800

    Add files via upload

---
+++ MMD-main/MMD-main/MalabonMed/index.php
@@ -0,0 +1,30 @@
+<?php 
+    include("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MedicalMed</title>
+
+</head>
+<body>
+    <div class="container-fluid" >
+        <div class="container-md">
+            <div class="carousel container-fluid p-5" >
+                <div class="container-md py-5">
+                    <h1>
+                        Convenient Appointment<br> 
+                        Scheduling for Malabon<br>
+                        Barangay Clinics!<br>
+                    </h1>
+                    <p>Reserve Your Spot with MalabonMed</p>
+                    <button type="button" class="btn btn-primary">Reserve Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
