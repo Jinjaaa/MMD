876364e67751b9ec9e6683539dede21d25f5c310
Jinjaaa <jinjer.ico@gmail.com>
2024-12-09 03:08:06 +0800

    Update 01
    tinuloy ko yung home page

--- MMD-main/MMD-main/MalabonMed/index.php
+++ MMD-main/MMD-main/MalabonMed/index.php
@@ -1,30 +1,129 @@
-<?php 
-    include("header.html");
+<?php
+include("header.html");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Tint&family=Lexend:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MedicalMed</title>
-
 </head>
+
 <body>
-    <div class="container-fluid" >
+    <div class="container-fluid pb-5">
         <div class="container-md">
-            <div class="carousel container-fluid p-5" >
-                <div class="container-md py-5">
-                    <h1>
-                        Convenient Appointment<br> 
+            <!--Blue Heading Start-->
+            <div class="carousel radius-5 container-fluid p-5 bg-warning rounded-5 shadow mb-5">
+                <div class="container-md">
+                    <h1 class="title_1">
+                        Convenient Appointment<br>
                         Scheduling for Malabon<br>
                         Barangay Clinics!<br>
                     </h1>
                     <p>Reserve Your Spot with MalabonMed</p>
-                    <button type="button" class="btn btn-primary">Reserve Now</button>
+                    <button type="button" class="btn btn1 btn-primary ">Reserve Now</button>
+                    <div class="container text-center pt-5">
+                        <div class="row">
+                            <div class="col">
+                                21<br>
+                                Malabon Barangays
+                            </div>
+                            <div class="col">
+                                1.5k<br>
+                                Good Reviews
+                            </div>
+                            <div class="col">
+                                2.5k<br>
+                                User Coming Back
+                            </div>
+                            <div class="col">
+                                90%<br>Feedback From User
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <!--Blue Heading End-->
+
+            <!--About start---->
+            <div class=" container-md  text-center mb-5">
+                <h2 class="title_2">About MalabonMed</h2>
+            </div>
+            <div class="row my-5">
+                <div class="col-sm-4 mb-3 mb-sm-0">
+                    <div class="card rounded-2">
+                        <div class="card-body text-center   shadow  rounded-2 transparent-bg">
+                            <img src="assets/environment.png" class="card-img-top w-25 " alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Convenience</h5>
+                                <p class="card-text text-dark text-opacity-75"> Platform for residents to easily schedule appointments at their local barangay clinics.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4  mb-3 mb-sm-0">
+                    <div class="card rounded-2">
+                        <div class="card-body text-center  shadow rounded-2 transparent-bg">
+                            <img src="assets/task-management.png" class="card-img-top w-25" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Management</h5>
+                                <p class="card-text text-dark text-opacity-75">Helps barangay clinics manage their schedules more efficiently.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-3 mb-sm-0">
+                    <div class="card rounded-2">
+                        <div class="card-body text-center   shadow rounded-2 transparent-bg">
+                            <img src="assets/info.png" class="card-img-top w-25" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Information</h5>
+                                <p class="card-text text-dark text-opacity-75">Contribute to a healthier community and encourage health management.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!---About end---->
+
+            <!--News Start-->
+            <div class="about container-md  text-center mb-5">
+                <h2 class="title_2">See The Latest News</h2>
+            </div>
+            <div class="card-group">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <!--News End-->
         </div>
     </div>
 </body>
+
 </html>
