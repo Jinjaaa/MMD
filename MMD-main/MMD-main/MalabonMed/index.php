c5c5fd54aaeeb8a579d07c54b526f94d2319dae2
Jinjaaa <jinjer.ico@gmail.com>
2024-12-09 18:59:37 +0800

    Update 2

--- MMD-main/MMD-main/MalabonMed/index.php
+++ MMD-main/MMD-main/MalabonMed/index.php
@@ -52,7 +52,11 @@ include("header.html");
 
             <!--About start---->
             <div class=" container-md  text-center mb-5">
-                <h2 class="title_2">About MalabonMed</h2>
+                <h2 class="title_2 text-start">
+                    Know More About Us
+                </h2>
+                <p class="text-start opacity-75" style="color: rgb(37, 37, 103);">Making it easy for users to schedule their visits quickly.</p>
+
             </div>
             <div class="row my-5">
                 <div class="col-sm-4 mb-3 mb-sm-0">
@@ -92,36 +96,93 @@ include("header.html");
             <!---About end---->
 
             <!--News Start-->
-            <div class="about container-md  text-center mb-5">
-                <h2 class="title_2">See The Latest News</h2>
-            </div>
-            <div class="card-group">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            <div class="container-md  py-5 ">
+                <div class="about container-md py-5">
+                    <h2 class="title_2 ">
+                        See The Latest News
+                    </h2>
+                    <p style="color: rgb(37, 37, 103);" class="opacity-75">Making it easy for users to schedule their visits quickly.</p>
+                </div>
+                <div class="card-group">
+                    <div class="card ">
+                        <img src="assets/n1.jpg" class="card-img-top " alt="...">
+                        <div class="card-body"><a href="https://web.facebook.com/photo/?fbid=475143325269757&set=a.126499470134146">
+                                <h5 class="card-title">BABALA SA LEPTOSPIROSIS</h5>
+                                <p class="card-text text-dark text-opacity-75">Huwag mag-atubiling bumisita sa MHMC para sa agarang konsultasyon at pag-gamot. Ang kalusugan mo ay mahalaga, magpa-konsulta na!!</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 1 week ago</small></p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="assets/n2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body"><a href="https://web.facebook.com/MHMC.Official/posts/pfbid02993S1RXE15xHv9YUH7BhgRqD1Sgty359W1M6yGdtDgzRzv6urMgHs1msyM9b1LLil">
+                                <h5 class="card-title">WE ARE HIRING</h5>
+                                <p class="card-text text-dark text-opacity-75">We are hiring! Submit your resume to humanresource@mhmc.com.ph or scan the QR code below!</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 6 days ago</small></p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="assets/n3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body"><a href="https://web.facebook.com/MHMC.Official/posts/pfbid01JBafJJxwCuCBeEUuvw3hxMiXDCMtQ2NMZC454S5PzqCdTBdRh6otEbHANRp8WWvl">
+                                <h5 class="card-title">Be a Lifesaver, Donate Blood!</h5>
+                                <p class="card-text text-dark text-opacity-75">Sa darating na December 14, 2024 (Sabado) magsasagawa ang Malabon Hospital and Medical Center katuwang ang Philippine Red Cross Malabon Chapte</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 5 days ago</small></p>
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
+                <p class="text-center py-5">
+                    <a class="icon-link icon-link-hover" href="#">See More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                        </svg>
+                    </a>
+                </p>
             </div>
             <!--News End-->
+
+            <!--Appointment Start-->
+            <div class="container-fluid  py-5">
+                <div class="container-md">
+                    <div class="appointment radius-5 container-fluid rounded-5  mb-5">
+                        <div class="container-md">
+                            <h2 class="title_2">
+                                Appointment Features
+                            </h2>
+                            <p style="color: rgb(37, 37, 103);" class="opacity-75">Making it easy for users to schedule their visits quickly.</p>
+                            <div class="container text-center pt-5">
+                                <div class="row">
+                                    <div class="col">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                                            <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0" />
+                                            <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                                        </svg>
+                                        <b>Selection of Service</b><br>
+                                        Allow users to choose the type of service
+                                    </div>
+                                    <div class="col">
+                                        <b>Cancel Anytime</b><br>
+                                        Allow users to cancel/reschedule the appointment
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <b>Appointment Selection</b><br>
+                                        Users can select their preferred date and time.
+                                    </div>
+                                    <div class="col">
+                                        <b>Reminder Notification</b><br>
+                                        Reminders for upcoming appointments. These can be sent via email or SMS.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!--Appointment End-->
         </div>
     </div>
 </body>
